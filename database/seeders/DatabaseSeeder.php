1e27cf6b278a82f45fe7936bee3fb4317b5aedb9
boyank28 <boyank28@gmail.com>
2024-03-11 04:07:56 +0700

    init project fic 15

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'boyank@fic15.com',
+            'role' => 'admin',
+            'password' => Hash::make('12345678'),
+            'phone' => '1234567890',
+        ]);
+
+        //seeder profile_clinics manual
+        \App\Models\ProfileClinic::factory()->create([
+            'name' => 'Klinik Boyank',
+            'address' => 'Jl. Purwasari Indah No. D4',
+            'phone' => '1234567890',
+            'email' => 'dr.boyank@klinik.com',
+            'doctor_name' => 'Dr. Boyank',
+            'unique_code' => '12345678',
+
+        ]);
+
+        //call
+        $this->call(DoctorSeeder::class);
+    }
+}
